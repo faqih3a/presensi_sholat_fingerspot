8329cdc734fae7f6c4f31e0995de7af4427dec05
faqih3a <ahmadfaqih3a@gmail.com>
2026-06-06 01:48:25 +0700

    update attlog

--- public/webhook/get_attlog.php
+++ public/webhook/get_attlog.php
@@ -4,11 +4,13 @@ $url = 'https://developer.fingerspot.io/api/get_attlog';
 $data = '{"trans_id":"1", "cloud_id":"S118001290", "start_date":"2026-05-30", "end_date":"2026-06-30"}';
 $authorization = "Authorization: Bearer FLDYHON2Z9S53DQ9";
 
+$postdata = $data;
+
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
