a172297bf98cb3120e80e5946347fdea66156061
faqih3a <ahmadfaqih3a@gmail.com>
2026-06-06 21:59:45 +0700

    update attlog

---
+++ public/store.php
@@ -0,0 +1,29 @@
+<?php
+    include 'include/global.php';
+    include 'include/function.php';
+
+    $original_data  = file_get_contents('php://input');
+    $decoded_data   = json_decode($original_data, true);
+    $encoded_data   = json_encode($decoded_data);
+
+    if (isset($decoded_data['type']) && isset($decoded_data['cloud_id']))
+    {
+
+        $type       = $decoded_data['type'];
+        $cloud_id   = $decoded_data['cloud_id'];
+        $created_at = date('Y-m-d H:i:s');
+
+        try {
+            $stmt = $conn->prepare("INSERT INTO t_log (cloud_id, type, created_at, original_data) VALUES (:cloud_id, :type, :created_at, :original_data)");
+            $stmt->execute([
+                ':cloud_id' => $cloud_id,
+                ':type' => $type,
+                ':created_at' => $created_at,
+                ':original_data' => $encoded_data
+            ]);
+        } catch (PDOException $e) {
+            // Log or handle insert error
+        }
+
+    }
+?>
